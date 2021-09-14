f89a488215aa9e10881eda6e9cee1765706c4e3b
Louie <sky85245630@gmail.com>
2021-09-14 18:07:23 +0800

    transition

--- nuxt.config.js
+++ nuxt.config.js
@@ -47,7 +47,7 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  css: ['~/assets/transition.css'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],

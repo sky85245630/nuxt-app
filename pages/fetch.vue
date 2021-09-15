d76cf2c69b30dd3a69729ee910fe0bce4f4d2440
Louie <sky85245630@gmail.com>
2021-09-15 16:56:50 +0800

    vuex store

---
+++ pages/fetch.vue
@@ -0,0 +1,16 @@
+<template>
+  　　<button @click="$store.commit('increment')">
+    {{ $store.state.counter }}
+  </button>
+</template>
+
+<script>
+export default {
+  fetch({ store, params }) {
+    const asd = 'fetch page'
+    store.commit('setResult', asd)
+    console.log('store', store)
+    console.log('params', params)
+  },
+}
+</script>
--- pages/product.vue
+++ pages/product.vue
@@ -7,16 +7,33 @@
         <a href="#" target="_blank" class="button--green">加入購物車</a>
         <a href="#" target="_blank" class="button--grey">立刻購買</a>
         <nuxt-link to="/login"><button>登入頁</button></nuxt-link>
+        {{ result }}
       </div>
     </div>
   </section>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   loading: false,
   head: {
     title: '商品',
+  },
+  data() {
+    return { result: '未查詢' }
+  },
+
+  // asyncData() {
+  //   return { result1: '報名結果是 ooxx' }
+  // },
+  async asyncData() {
+    let { data } = await axios.post('https://www.thef2e.com/api/isSignUp', {
+      email: 'hexscholl@test.com',
+    })
+
+    return { result: data }
   },
 }
 </script>
